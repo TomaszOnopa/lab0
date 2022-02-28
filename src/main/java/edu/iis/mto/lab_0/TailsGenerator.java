43d0ae06d9698e3e337fc6141376d574251428a0
Onopa.Tomasz <228818@edu.p.lodz.pl>
2022-02-28 17:55:52 +0100

    wszystkie testy zaliczone

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -7,7 +7,7 @@ public class TailsGenerator {
 
     public List<String> tails(String value) {
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i<=value.length(); i++) list.add("");
+        for (int i = 0; i<=value.length(); i++) list.add(value.substring(i));
         return list;
     }
 
